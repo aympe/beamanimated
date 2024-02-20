0b31073f6f255b7ac1e426d323143b7ecb61623d
aympe <59691588+aympe@users.noreply.github.com>
2024-02-20 23:03:41 +0900

    w

--- beam/src/App.js
+++ beam/src/App.js
@@ -1,7 +1,7 @@
 "use client";
 
-import { Icons } from "./icons";
-import { cn } from "../../lib/utils";
+import { Icons } from "./components/icons";
+import { cn } from "./lib/utils";
 import { AnimatedBeam } from "src/components/magicui/animated-beam";
 import React, { forwardRef, useRef } from "react";
 
